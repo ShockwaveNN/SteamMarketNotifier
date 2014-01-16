2414ac153a22ef30fa28cac711c4edd1107c198b
Pavel.Lobashov <dr-evil-lob@mail.ru>
2014-01-16 22:23:58 +0400

    Rework almost all Desperatly need classes

--- main.rb
+++ main.rb
@@ -1,25 +1,66 @@
 #encoding: utf-8
 require 'open-uri'
 require 'nokogiri'
+require 'gmail'
+require 'json'
+
+USD_IN_RUB = 0.03
+USD_IN_EUR = 1.36
+URL_ITEM = 'http://steamcommunity.com/market/listings/753/33680-Princess%20Nehema'
 
 def get_page(url)
   Nokogiri::HTML(open(url))
 end
 
-def get_price_from_cheapes(xml, number = 0)
-  xml.xpath("(//div[@class='market_listing_right_cell market_listing_their_price'])[#{number + 1}]/span/span[1]")[0].text.to_s.scan(/[[:print:]]/).join.gsub('€ ', '').gsub(',', '.').to_f
+def get_prices(xml)
+  prices_array = []
+  (0..9).each do |i|
+    prices_array << xml.xpath("(//div[@class='market_listing_right_cell market_listing_their_price'])[#{i + 1}]/span/span[1]")[0].text.to_s.scan(/[[:print:]]/).join.gsub(',', '.')
+  end
+  prices_array
 end
 
 
-while 1
-  xml = get_page('http://steamcommunity.com/market/listings/753/42140-Princess%20Mikunda')
-  first_price = get_price_from_cheapes(xml)
-  second_price = get_price_from_cheapes(xml, 2)
-  diff = second_price - first_price
-  percents = diff / second_price
-  if percents > 30
-    print "\a"
-    p first_price
+def convert_price(prices_array)
+  converted = []
+  prices_array.each do |cur_price|
+    price = nil
+    cur_price.gsub!('--', '00')
+    if cur_price.include?('pуб')
+      price_rub = cur_price.gsub(' pуб.', '').to_f
+      price = price_rub * USD_IN_RUB
+    elsif cur_price.include?('€')
+      price_rub = cur_price.gsub('€ ', '').to_f
+      price = price_rub * USD_IN_EUR
+    elsif cur_price.include?('USD')
+      price_rub = cur_price.gsub(' USD', '').gsub('$', '').to_f
+      price = price_rub
+    end
+    converted << price
   end
-  sleep 60
+  converted
+end
+
+def notify
+  settings_string = File.open('Mail.config').read
+  setttings = JSON[settings_string]
+  gmail = Gmail.new(setttings['mail'], setttings['password'])
+  email = gmail.generate_message do
+    to "shockwavenn@gmail.com"
+    subject "Steam Market Notifier"
+    body URL_ITEM
+  end
+  email.deliver!
+  gmail.logout
+end
+
+notify
+while 1
+  xml = get_page(URL_ITEM)
+  prices = get_prices(xml)
+  converted = convert_price(prices)
+  if converted[0] / converted[1] < 0.99
+   notify
+  end
+  sleep 10
 end
